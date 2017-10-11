9e2c67d903638d95e452bc85d801841668663648
陈志同 <chenzhitong12@qq.com>
2017-10-11 17:49:20 +0800

    添加回到顶部，修改时间筛选器的样式

--- NeoWeb/wwwroot/js/blog.js
+++ NeoWeb/wwwroot/js/blog.js
@@ -8,4 +8,15 @@
         _this.find("ul").toggle(300);
     });
 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            $('.back-to-top').fadeIn(100);
+        } else {
+            $('.back-to-top').fadeOut(100);
+        }
+    });
+
+    $('.back-to-top').click(function (event) {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+    })    
 })
