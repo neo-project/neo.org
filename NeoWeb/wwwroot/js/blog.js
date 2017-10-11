03d38ab3732ead0159478887495760036f34a4be
YanMingxia <18605540702@163.com>
2017-10-11 10:52:20 +0800

    blog内容页面样式及相应html，js 修复了部分blog页面bug

--- NeoWeb/wwwroot/js/blog.js
+++ NeoWeb/wwwroot/js/blog.js
@@ -3,7 +3,7 @@
     $("#blogDate li a.iconfont").click(function () {
         var _this = $(this).parent("li");
         _this.siblings().find("ul").hide();
-        $(".selected-part").removeClass();
+        $(".selected-part").removeClass("selected-part");
         _this.addClass("selected-part");
         _this.find("ul").toggle(300);
     });
