88e3d59566b6eda20a2581878abe3f6abf7b825a
陈志同 <chenzhitong12@hotmail.com>
2018-01-28 01:11:05 +0800

    升级Bootstrap到4.0

--- NeoWeb/wwwroot/js/site.js
+++ NeoWeb/wwwroot/js/site.js
@@ -1,13 +1,13 @@
 ﻿function resize() {
     for (var i = 0; i < 10 && $("#menu_list").width() + $("#logo_div").width() > $("#nav_container").width(); i++) {
         $("#dropdown_btn").show();
-        $("#more_list").prepend($(".menu-item:last"));
-        $(".menu-item:last").removeClass("menu-item");
+        $("#more_list").prepend($(".nav-item:last"));
+        $(".nav-item:last").removeClass("nav-item");
     }
     if ($("#menu_list").width() + $("#logo_div").width() + 100 <= $("#nav_container").width()) {
         var preShowItem = $("#more_list li:first");
         $("#dropdown_btn").before(preShowItem);
-        preShowItem.addClass("menu-item");
+        preShowItem.addClass("nav-item");
         if ($("#more_list li").length == 0) {
             $("#dropdown_btn").hide();
         }
