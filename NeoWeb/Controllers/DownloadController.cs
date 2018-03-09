a623bf680b299bbc7dc38b17674e4ec21cdfa987
陈志同 <chenzhitong12@hotmail.com>
2018-03-09 15:17:50 +0800

    Add Feature: Show date of offline synchronized data. (#641)

--- NeoWeb/Controllers/DownloadController.cs
+++ NeoWeb/Controllers/DownloadController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace NeoWeb.Controllers
 {
@@ -7,6 +8,11 @@ namespace NeoWeb.Controllers
         // GET: Download  
         public IActionResult Index()
         {
+            FileInfo fi = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/client/chain.acc.zip"));
+            ViewBag.MainNetCreationTime = fi.Exists ? fi.LastWriteTime.ToString() : "";
+
+            fi = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/client/chain.acc.test.zip"));
+            ViewBag.TestNetCreationTime = fi.Exists ? fi.LastWriteTime.ToString() : "";
             return View();
         }
     }
