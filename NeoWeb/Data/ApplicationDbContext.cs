a02ad0f852d60b11b1bdc0c9cdb6f74812da3905
陈志同 <chenzhitong12@hotmail.com>
2018-04-08 14:20:43 +0800

    Feature/awards (#655)
    * 开发者大赛获奖页面
    
    * 修改大赛页面，获奖作品页面js冲突
    
    * 修改获奖页面样式，语言兼容
    
    * 调整获奖页面兼容性
    
    * 获奖页面样式修改完善
    
    * 中英文切换按钮添加
    
    * 添加作品展示
    
    * update
    
    * 修改名次
    
    * 开发者大赛页面修改

--- NeoWeb/Data/ApplicationDbContext.cs
+++ NeoWeb/Data/ApplicationDbContext.cs
@@ -21,6 +21,8 @@ namespace NeoWeb.Data
 
         public DbSet<News> News { get; set; }
 
+        public DbSet<Media> Media { get; set; }
+
         public DbSet<ICO1> ICO1 { get; set; }
 
         public DbSet<ICO2> ICO2 { get; set; }
