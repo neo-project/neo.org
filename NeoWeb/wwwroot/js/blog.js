2cafd9349f49e0294e9e592a3285fd525a1cde95
YanMingxia <18605540702@163.com>
2018-06-07 15:38:58 +0800

    blog页面优化

--- NeoWeb/wwwroot/js/blog.js
+++ NeoWeb/wwwroot/js/blog.js
@@ -5,14 +5,6 @@
         $(this.parentNode).find("ul").show(300);
     });
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 150) {
-            $('.back-to-top').fadeIn(100);
-        } else {
-            $('.back-to-top').fadeOut(100);
-        }
-    });
-
     $('.back-to-top').click(function (event) {
         $('html, body').animate({ scrollTop: 0 }, 500);
     })
