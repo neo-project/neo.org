b73cc00db6bcf085c081a66288dde180be1737c1
YanMingxia <18605540702@163.com>
2018-06-29 14:28:20 +0800

    页面样式修改

--- NeoWeb/wwwroot/js/candidate.js
+++ NeoWeb/wwwroot/js/candidate.js
@@ -1,20 +1,25 @@
 //点击展开
+$(".more-info").click(function () {
+    $(this).parents(".tr-con").next(".can-detail").toggle("slow");
+})
 
 //每秒获取区块高度、出块时间计算
-var json = { 'jsonrpc': '2.0', 'method': 'getblockcount', 'params': [], 'id': 1 };
-var str = JSON.stringify(json);
-$.ajax({
-    type: 'POST',
-    url: "http://seed2.neo.org:10332",
-    data: str,
-    success: function () {
-        var html = template(document.getElementById('tableContent').innerHTML, dae);
-        document.getElementById('tableList').innerHTML = html;
-    },
-    fail: function () {
-        alert("fail");
-    }
-});
+function getNum() {
+    var json = { 'jsonrpc': '2.0', 'method': 'getblockcount', 'params': [], 'id': 1 };
+    var str = JSON.stringify(json);
+    $.ajax({
+        type: 'POST',
+        url: "http://seed2.neo.org:10332",
+        data: str,
+        success: function () {
+            var html = template(document.getElementById('tableContent').innerHTML, dae);
+            document.getElementById('tableList').innerHTML = html;
+        },
+        fail: function () {
+            alert("fail");
+        }
+    });
+}
 
 //点击刷新页面
 
@@ -24,19 +29,10 @@ $.ajax({
 var myChart = echarts.init(document.getElementById('main'));
 option = {
     backgroundColor: '#F9FBF2',
-    title: {
-        text: '请求数',
-        textStyle: {
-            fontWeight: 'normal',
-            fontSize: 16,
-            color: '#F1F1F3'
-        },
-        left: '6%'
-    },
     tooltip: {
         trigger: 'axis',
         backgroundColor: 'rgba(255,255,255,1)',
-        borderRadius:'2',
+        borderRadius: '2',
         axisPointer: {
             lineStyle: {
                 color: '#999999'
@@ -53,16 +49,16 @@ option = {
         itemHeight: 14,
         itemGap: 14,
         data: ['交易量', '区块大小'],
-        right: '4%',
+        right: '0',
         textStyle: {
             fontSize: 12,
             color: '#666666'
         }
     },
     grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
+        left: '0',
+        right: '0',
+        bottom: '0',
         containLabel: true
     },
     xAxis: [{
@@ -71,17 +67,37 @@ option = {
         axisLine: {
             lineStyle: {
                 color: '#CCC',
-                opcity:'0.8'
+                opcity: '0.8'
             }
         },
         data: ['13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30', '13:35', '13:40', '13:45', '13:50', '13:55']
     }],
     yAxis: [{
         type: 'value',
-        name: '单位（%）',
+        name: '数量',
         axisTick: {
             show: false
         },
+        axisLine: {
+            lineStyle: {
+                color: '#CCC'
+            }
+        },
+        axisLabel: {
+            margin: 10,
+            textStyle: {
+                fontSize: 14
+            }
+        },
+        splitLine: {
+            lineStyle: {
+                color: '#ECECEC'
+            }
+        }
+    }, {
+        type: 'value',
+        scale: true,
+        name: 'KB',
         axisLine: {
             lineStyle: {
                 color: '#CCC'
@@ -103,9 +119,8 @@ option = {
         name: '交易量',
         type: 'line',
         smooth: true,
-        symbol: 'circle',
-        symbolSize: 5,
         showSymbol: false,
+        yAxisIndex: 0,
         lineStyle: {
             normal: {
                 width: 2
@@ -129,14 +144,13 @@ option = {
                 color: 'rgb(200,220,25)'
             }
         },
-        data: [220, 182, 191, 134, 150, 120, 110, 125, 145, 122, 165, 122]
+        data: [220, 182, 191, 134, 500, 120, 110, 125, 145, 122, 165, 122]
     }, {
         name: '区块大小',
         type: 'line',
         smooth: true,
-        symbol: 'circle',
-        symbolSize: 5,
         showSymbol: false,
+        yAxisIndex: 1,
         lineStyle: {
             normal: {
                 width: 2
@@ -160,7 +174,7 @@ option = {
                 color: 'rgb(170,203,162)'
             }
         },
-        data: [120, 110, 125, 145, 122, 165, 122, 220, 182, 191, 134, 150]
+        data: [120, 110, 15, 120, 122, 165, 122, 220, 182, 191, 134, 150]
     }]
 };
 // 使用刚指定的配置项和数据显示图表。
