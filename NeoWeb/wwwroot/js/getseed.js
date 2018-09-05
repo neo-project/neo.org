8d59190ed238135fafc80c1810683ea1f34fd752
Ming <18605540702@163.com>
2018-09-05 16:04:04 +0800

    共识页面修改 (#713)

--- NeoWeb/wwwroot/js/getseed.js
+++ NeoWeb/wwwroot/js/getseed.js
@@ -5,25 +5,23 @@ function getSeed() {
         url: "js/seedlist.json",
         dataType: "json",
         success: function (data) {
-            var blo_h = 0, url_s;
-            var json = { 'jsonrpc': '2.0', 'method': 'getblockcount', 'params': [], 'id': 1 };
-            var str = JSON.stringify(json);
+            var blo_h = 0;
             for (var i = 0; i < data.sites.length; i++) {
+                var json = { 'jsonrpc': '2.0', 'method': 'getblockcount', 'params': [], 'id': i};
+                var str = JSON.stringify(json);
                 var _url = data.sites[i].url + ":" + data.sites[i].port;
                 $.ajax({
                     type: 'POST',
                     url: _url,
                     data: str,
-                    async: false,
-                    success: function (data) {
-                        if (data.result >= blo_h) {
-                            blo_h = data.result;
-                            url_s = _url;
+                    success: function (re) {
+                        if (re.result >= blo_h) {
+                            blo_h = re.result;
+                            return send_url = data.sites[re.id].url + ":" + data.sites[re.id].port;
                         }
                     }
-                })
+                });
             }
-            send_url = url_s;
         },
         fail: function () {
             alert("fail");
