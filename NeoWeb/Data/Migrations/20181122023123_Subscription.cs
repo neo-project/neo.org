ef0f6aca2ab3e247960692ad04d5b4fd13d8e9b7
chenzhitong <chenzhitong12@qq.com>
2018-11-22 10:45:52 +0800

    添加邮件订阅的后台

---
+++ NeoWeb/Data/Migrations/20181122023123_Subscription.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NeoWeb.Data.Migrations
+{
+    public partial class Subscription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subscription",
+                columns: table => new
+                {
+                    Email = table.Column<string>(nullable: false),
+                    IsSubscription = table.Column<bool>(nullable: false),
+                    SubscriptionTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscription", x => x.Email);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscription");
+        }
+    }
+}
---
+++ NeoWeb/Models/SubscriptionModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoWeb.Models
+{
+    public class Subscription
+    {
+        [Key]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public bool IsSubscription { get; set; }
+        
+        public DateTime SubscriptionTime { get; set; }
+    }
+}
