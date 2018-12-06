1ceb4af7264ffc8b9b3dbc992db1de384308c224
YanMingxia <18605540702@163.com>
2018-12-06 18:16:22 +0800

    Devcon页面彻底完善

--- DevCon/wwwroot/js/devcon.js
+++ DevCon/wwwroot/js/devcon.js
@@ -1,20 +1,30 @@
 ﻿function submitE() {
     var reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$");
     var _em = $("#em").val();
-    $(".modal").modal('show');
     if(_em === ""){
-        // alert("输入不能为空!");
+        $("#hintDev").html("输出不能为空");
+        $(".modal").modal('show');
         return false;
     }else if(!reg.test(_em)){
-        // alert("验证不通过!");
+        $("#hintDev").html("请检查格式后重新输入");
+        $(".modal").modal('show');
         return false;
     }else{
         $.ajax({
             url: 'https://neo.org/subscription/add?email='+_em,
             type: "GET",
             success: function (data) {
-                console.log(data);
-
+                if(data=="true"){
+                    $("#hintDev").html("提交成功！");
+                    $(".modal").modal('show');
+                }else{
+                    $("#hintDev").html("请勿多次重复提交！");
+                    $(".modal").modal('show');
+                }
+            },
+            error:function () {
+                $("#hintDev").html("提交失败，请刷新页面后重试！");
+                $(".modal").modal('show');
             }
         });
         return true;
