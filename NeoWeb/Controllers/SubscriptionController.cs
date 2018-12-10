06b80e791f9172f5a37c73c1ac792998de779670
chenzhitong <chenzhitong12@qq.com>
2018-12-10 15:59:58 +0800

    update

--- NeoWeb/Controllers/SubscriptionController.cs
+++ NeoWeb/Controllers/SubscriptionController.cs
@@ -37,16 +37,16 @@ namespace NeoWeb.Controllers
             if (ModelState.IsValid)
             {
                 if (!Helper.CCAttack(_accessor.HttpContext.Connection.RemoteIpAddress, "consensus_post", 3600, 10))
-                    return "Protecting from overposting attacks now!";
+                    return "Protecting from overposting attacks now!"; //IP被禁止访问
                 subscription.IsSubscription = true;
                 subscription.SubscriptionTime = DateTime.Now;
                 if (_context.Subscription.Any(e => e.Email == subscription.Email))
-                    return "true";
+                    return "Email has been submitted, do not repeat the submission."; //重复提交
                 _context.Add(subscription);
                 _context.SaveChanges();
-                return "true";
+                return "Email sucessfully sumbitted!"; //成功
             }
-            return "false";
+            return "Please check you email format and entry again."; //格式错误
         }
     }
 }
