2f925544d7f8f2fd86f0200f7d1d301725bf5fde
chenzhitong <chenzhitong12@qq.com>
2018-12-18 00:20:05 +0800

    修复升级后多语言切换失效的Bug

--- NeoWeb/Startup.cs
+++ NeoWeb/Startup.cs
@@ -33,7 +33,7 @@ namespace NeoWeb
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
             services.AddLocalization(options => options.ResourcesPath = "Resources");
