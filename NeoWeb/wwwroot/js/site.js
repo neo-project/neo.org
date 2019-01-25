51f641930b5919e01f22a2c0710110bd6f6d3d62
chenzhitong <chenzhitong12@qq.com>
2019-01-25 17:33:57 +0800

    更新导航栏样式

--- NeoWeb/wwwroot/js/site.js
+++ NeoWeb/wwwroot/js/site.js
@@ -1,22 +1,4 @@
-﻿function resize() {
-    for (var i = 0; i < 10 && $("#menu_list").width() + $("#logo_div").width() > $("#nav_container").width(); i++) {
-        $("#dropdown_btn").show();
-        $("#more_list").prepend($(".nav-item:last"));
-        $(".nav-item:last").removeClass("nav-item");
-    }
-    if ($("#menu_list").width() + $("#logo_div").width() + 100 <= $("#nav_container").width()) {
-        var preShowItem = $("#more_list li:first");
-        $("#dropdown_btn").before(preShowItem);
-        preShowItem.addClass("nav-item");
-        if ($("#more_list li").length === 0) {
-            $("#dropdown_btn").hide();
-        }
-    }
-}
-resize();
-$(window).bind('resize', resize);
-
-function language(lang) {
+﻿function language(lang) {
     var rgExp = /\w{2}-\w{2}/;
 
     if (rgExp.exec(location.href)) {
