35a5c6ae9b623a2e27e02c23f48d4ef27b83d1a3
yanmingxia <18605540702@163.com>
2019-08-06 18:31:18 +0800

    typing动效

---
+++ NeoWeb/wwwroot/js/typetext.js
@@ -0,0 +1,35 @@
+﻿/*循环时间、最终状态、字符串个数、字符个数、两种定时器、
+ */
+typetext();
+function typetext(){
+    $(".typetext").html();
+    setTimeout(deleted, 800);
+}
+
+function typing(ele) {
+    ele = "ontes nascetur ridiculus mu";
+    var str = "";
+    for (var i = 0; i <= ele.length; i++) {
+        (function (i) {      //立刻执行函数
+            setTimeout(function () {
+                str = str + ele.charAt(i);
+                $(".typetext").html(str);
+            }, i * 250);
+        })(i); 
+    }
+}
+
+function deleted(ele) {
+    ele = $(".typetext").html();
+    var _l = ele.length;
+    var str = "";
+    for (var i = 0; i <= ele.length; i++) {
+        (function (i) {      //立刻执行函数
+            setTimeout(function () {
+                str = ele.substring(0, _l-i);
+                $(".typetext").html(str);
+                if (i == _l) setTimeout(typing, 500);
+            }, i * 60);
+        })(i);
+    }
+}
