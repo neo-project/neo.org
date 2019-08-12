efef33699b2655043d1a3acfcc6e2be05b31ad53
yanmingxia <18605540702@163.com>
2019-08-12 16:44:34 +0800

    新增eco controller（eco/ecoboost） 修改基础css样式

---
+++ NeoWeb/Controllers/EcoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NeoWeb.Controllers
+{
+    public class EcoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EcoBoost()
+        {
+            return View();
+        }
+    }
+}
