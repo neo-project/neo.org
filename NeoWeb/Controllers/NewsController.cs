207acb7888cb03a8e8d10e8b93b3d8b247fc479e
陈志同 <chenzhitong12@hotmail.com>
2019-08-26 10:53:59 +0800

    调整客户端页面截图的样式

--- NeoWeb/Controllers/NewsController.cs
+++ NeoWeb/Controllers/NewsController.cs
@@ -88,8 +88,6 @@ namespace NeoWeb.Controllers
         }
 
         // POST: news/edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,ChineseTitle,EnglishTitle,ChineseTags,EnglishTags,Link")] News news,
