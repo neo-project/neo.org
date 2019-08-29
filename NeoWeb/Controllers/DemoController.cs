7631c307ff42cbbe97f43f7b140e9aa5a1c9ad4c
yanmingxia <18605540702@163.com>
2019-08-29 10:30:33 +0800

    模板页面位置修改

---
+++ NeoWeb/Controllers/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NeoWeb.Controllers
+{
+    public class DemoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
