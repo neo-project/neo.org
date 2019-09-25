807e02c0899f8938e71dcf2a84fc9de7f4477820
yanmingxia <18605540702@163.com>
2019-09-25 14:07:53 +0800

    首页typing

--- NeoWeb/wwwroot/js/typetext.js
+++ NeoWeb/wwwroot/js/typetext.js
@@ -1,35 +1,69 @@
-﻿/*循环时间、最终状态、字符串个数、字符个数、两种定时器、
- */
-typetext();
-function typetext(){
-    $(".typetext").html();
-    setTimeout(deleted, 800);
+﻿
+/**
+* Created by Mingxia on 2019/9/25.
+* Typing
+*/
+
+function TypeText(options) {
+    options = options || {};
+    this.options = {
+        id: options.id || 'typetext',
+        text: options.text || ["TypeText"],
+        speed: options.speed || 200,
+        despeed: options.despeed || 60,
+        space: options.space || 500
+    };
+
+    this.ele = document.getElementById(this.options.id);
+    this.html = this.ele.innerHtml;
+    this.text = this.options.text;
+    this.speed = this.options.speed;
+    this.despeed = this.options.despeed;
+    this.space = this.options.space;
+
+    this.init();
 }
 
-function typing(ele) {
-    ele = "Most Community Driven";
-    var str = "";
-    for (var i = 0; i <= ele.length; i++) {
-        (function (i) {      //立刻执行函数
-            setTimeout(function () {
-                str = str + ele.charAt(i);
-                $(".typetext").html(str);
-            }, i * 200);
-        })(i); 
-    }
-}
+TypeText.prototype = {
+    deleted: function () {
+        var ele = $("#typetext").html();
+        var _l = ele.length;
+        var type = "";
+        for (var i = 0; i <= ele.length; i++) {
+            (function (i) {
+                setTimeout(function () {
+                    type = ele.substring(0, _l - i);
+                    document.getElementById("typetext").innerText = type;
+                }, i * 60);
+            })(i);
+        }
+    },
+    typing: function (str) {
+        var type = "";
+        for (var i = 0; i <= str.length; i++) {
+            (function (i) {      //立刻执行函数
+                setTimeout(function () {
+                    type = type + str.charAt(i);
+                    document.getElementById("typetext").innerText = type;
+                }, i * 200);
+            })(i);
+        }
+    },
+    getarr: function () {
+        var last = 0, de = 0;
+        this.text.forEach((item, index) => {
+            de = last + item.length * this.speed + this.space;
+            setTimeout(this.typing, last, item);
+            last = last + item.length * (this.despeed + this.speed) + this.space * 2;
+            if (index !== this.text.length - 1) {
+                setTimeout(this.deleted, de);
+            }
+        });
+    },
+    hastext: function () {
 
-function deleted(ele) {
-    ele = $(".typetext").html();
-    var _l = ele.length;
-    var str = "";
-    for (var i = 0; i <= ele.length; i++) {
-        (function (i) {      //立刻执行函数
-            setTimeout(function () {
-                str = ele.substring(0, _l-i);
-                $(".typetext").html(str);
-                if (i == _l) setTimeout(typing, 500);
-            }, i * 60);
-        })(i);
+    },
+    init: function () {
+        this.getarr();
     }
 }
