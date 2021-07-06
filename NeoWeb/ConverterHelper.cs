f821b7f61aa4fc00f8a0d7214aa688ac26d68774
陈志同 <bitcoin2077@outlook.com>
2021-07-06 16:30:12 +0800

    Update ConverterHelper.cs

--- NeoWeb/ConverterHelper.cs
+++ NeoWeb/ConverterHelper.cs
@@ -444,7 +444,7 @@ namespace NeoWeb
                     result.Add($"{op}");
                 }
             }
-            return result.ToArray();
+            return result.ToArray().ToList();
         }
     }
 }
