25372009a3fc62fc3a90eb01ae399567367e7e6a
Apisit Toompkadee <apisit@olto.co>
2022-03-24 19:26:02 +0900

    move tailwind to root

--- tailwind_debug.sh
+++ tailwind_debug.sh
@@ -1,1 +1,1 @@
-npx tailwindcss -i ./NeoWeb/wwwroot/css/tailwind.css -o ./NeoWeb/wwwroot/css/tailwind.output.css --watch
+npx tailwindcss -i ./tailwind.css -o ./NeoWeb/wwwroot/css/tailwind.output.css --watch
--- tailwind_prod.sh
+++ tailwind_prod.sh
@@ -1,1 +1,1 @@
-npx tailwindcss -i ./NeoWeb/wwwroot/css/tailwind.css -o ./NeoWeb/wwwroot/css/tailwind.output.css --minify
+npx tailwindcss -i ./tailwind.css -o ./NeoWeb/wwwroot/css/tailwind.output.css --minify
