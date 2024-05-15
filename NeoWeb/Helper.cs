df73b0bc921cf5f57533c88506a416c594211025
陈志同 <bitcoin2077@outlook.com>
2024-05-15 16:50:22 +0800

    load draft

--- NeoWeb/Helper.cs
+++ NeoWeb/Helper.cs
@@ -241,19 +241,6 @@ namespace NeoWeb
             return true;
         }
 
-        public static bool VerifySignature(string message, string signature, string pubkey)
-        {
-            var msg = Encoding.Default.GetBytes(message);
-            try
-            {
-                return VerifySignature(msg, signature.HexToBytes(), pubkey.HexToBytes());
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
         public static byte[] HexToBytes(this string value)
         {
             if (value == null || value.Length == 0)
@@ -264,40 +251,6 @@ namespace NeoWeb
             for (int i = 0; i < result.Length; i++)
                 result[i] = byte.Parse(value.Substring(i * 2, 2), NumberStyles.AllowHexSpecifier);
             return result;
-        }
-
-        //reference https://github.com/neo-project/neo/blob/master/neo/Cryptography/Crypto.cs
-        public static bool VerifySignature(byte[] message, byte[] signature, byte[] pubkey)
-        {
-            if (pubkey.Length == 33 && (pubkey[0] == 0x02 || pubkey[0] == 0x03))
-            {
-                try
-                {
-                    pubkey = ThinNeo.Cryptography.ECC.ECPoint.DecodePoint(pubkey, ThinNeo.Cryptography.ECC.ECCurve.Secp256r1).EncodePoint(false).Skip(1).ToArray();
-                }
-                catch
-                {
-                    return false;
-                }
-            }
-            else if (pubkey.Length == 65 && pubkey[0] == 0x04)
-            {
-                pubkey = pubkey.Skip(1).ToArray();
-            }
-            else if (pubkey.Length != 64)
-            {
-                throw new ArgumentException("pubkey is incorrect.");
-            }
-            using var ecdsa = ECDsa.Create(new ECParameters
-            {
-                Curve = ECCurve.NamedCurves.nistP256,
-                Q = new ECPoint
-                {
-                    X = pubkey.Take(32).ToArray(),
-                    Y = pubkey.Skip(32).ToArray()
-                }
-            });
-            return ecdsa.VerifyData(message, signature, HashAlgorithmName.SHA256);
         }
 
         /// <summary>
@@ -345,6 +298,7 @@ namespace NeoWeb
             ip.GetAddressBytes().ToList().ForEach(p => o += (long)p << 8 * x--);
             return o;
         }
+
         public static string Sanitizer(string input)
         {
             input = Replace(input, @"<!\-\-\[if gte mso 9\]>[\s\S]*<!\[endif\]\-\->", ""); //删除 ms office 注解
